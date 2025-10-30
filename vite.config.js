56bbda6d5494f467ec9ccfd6889ffba5251eea1c
bhanudova02 <bhanudova02@gmail.com>
2025-10-31 00:31:01 +0530

    config change added for accesing mobile

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  server: {
+    host: true, // 👈 This enables access from mobile (your local IP)
+    port: 5173, // optional (can be any port)
+  },
 })
